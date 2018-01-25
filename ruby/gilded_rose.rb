c47cf6da4670c1a79f769e8fc91f19f988ae0e78
miguelff <miguelff@github.com>
2018-01-25 12:35:17 +0100

    Prepare an alternative path for the legacy code
    Meanwhile, keep update_quality equivalent

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,47 +6,55 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != SpecialItems::AGED_BRIE and item.name != SpecialItems::BACKSTAGE_PASSES
-        if item.quality > 0
-          if item.name != SpecialItems::SULFURAS
-            item.quality = item.quality - 1
+      if item.name == SpecialItems::AGED_BRIE
+        legacy_update_quality item
+      else
+        legacy_update_quality item
+      end
+    end
+  end
+
+  def legacy_update_quality(item)
+    if item.name != SpecialItems::AGED_BRIE and item.name != SpecialItems::BACKSTAGE_PASSES
+      if item.quality > 0
+        if item.name != SpecialItems::SULFURAS
+          item.quality = item.quality - 1
+        end
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
+        if item.name == SpecialItems::BACKSTAGE_PASSES
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
           end
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
+          end
+        end
+      end
+    end
+    if item.name != SpecialItems::SULFURAS
+      item.sell_in = item.sell_in - 1
+    end
+    if item.sell_in < 0
+      if item.name != SpecialItems::AGED_BRIE
+        if item.name != SpecialItems::BACKSTAGE_PASSES
+          if item.quality > 0
+            if item.name != SpecialItems::SULFURAS
+              item.quality = item.quality - 1
+            end
+          end
+        else
+          item.quality = item.quality - item.quality
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == SpecialItems::BACKSTAGE_PASSES
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != SpecialItems::SULFURAS
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != SpecialItems::AGED_BRIE
-          if item.name != SpecialItems::BACKSTAGE_PASSES
-            if item.quality > 0
-              if item.name != SpecialItems::SULFURAS
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
         end
       end
     end
