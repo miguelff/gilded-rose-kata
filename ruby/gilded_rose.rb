84da34b61e562189e65024827e4afd742446117c
miguelff <miguelff@github.com>
2018-01-25 12:31:21 +0100

    Refactoring, DRY magic constants

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,16 +6,16 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != SpecialItems::AGED_BRIE and item.name != SpecialItems::BACKSTAGE_PASSES
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          if item.name != SpecialItems::SULFURAS
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name == SpecialItems::BACKSTAGE_PASSES
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -29,14 +29,14 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      if item.name != SpecialItems::SULFURAS
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != SpecialItems::AGED_BRIE
+          if item.name != SpecialItems::BACKSTAGE_PASSES
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              if item.name != SpecialItems::SULFURAS
                 item.quality = item.quality - 1
               end
             end
