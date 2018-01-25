3b586189a179bf413cbb2b0bdfc6bba81180e7cc
miguelff <miguelff@github.com>
2018-01-25 11:53:00 +0100

    User correct rspec 2 idioms

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -2,12 +2,15 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      items[0].name.should == "fixme"
-    end
+  def update_quality(*items)
+    GildedRose.new(items).update_quality
   end
 
+  describe "General requirements:" do
+    it "Does not change the name" do
+      item = Item.new("foo", 0, 0)
+      update_quality item
+      expect(item.name).to eq("foo")
+    end
+  end
 end
